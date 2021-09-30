00017a6d62410c1632c45331df7fb84ac5ccad1c
Yash Atrey <atreyyash@gmail.com>
2021-09-30 11:10:37 +0530

    exp4

---
+++ ADA_Lab/Exp-4/a/LinearSearch.java
@@ -0,0 +1,29 @@
+/**
+ * LinearSearch
+ */
+import java.util.*;
+
+public class LinearSearch{
+    public static int linear_search(int[] arr, int idx, int x){
+        if(idx == arr.length){
+            return -1;
+        }
+        if(arr[idx] == x){
+            return idx;
+        }
+        int data = linear_search(arr, idx+1, x);
+        return data;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = s.nextInt();
+        }
+        int x = s.nextInt();
+        System.out.println(linear_search(arr, 0, x));
+    }
+}
---
+++ ADA_Lab/Exp-4/b/BinarySearch.java
@@ -0,0 +1,39 @@
+/**
+ * BinarySearch
+ */
+import java.util.*;
+
+public class BinarySearch{
+    public static void binary_search(int[] arr, int x){
+        int low = 0;
+        int high = arr.length-1;
+        while(low<high){
+            int mid = (low+high) / 2;
+            if(arr[mid] == x){
+                System.out.println("Array found at : "+(mid+1));
+                return;
+            }
+            else if(arr[mid]> x){
+                high = mid - 1;
+            }
+            else if(arr[mid]< x){
+                low = mid + 1;
+            }
+            else{
+                System.out.println("Element not found");
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = s.nextInt();
+        }
+        int x = s.nextInt();
+        binary_search(arr, x);
+    }
+}
